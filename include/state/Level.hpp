b39afcba9f6ff4114c07740dadf87096819cbb6b
kimci86 <kimci86@hotmail.fr>
2017-10-29 11:06:06 +0100

    Rewrite Level state

--- include/state/Level.hpp
+++ include/state/Level.hpp
@@ -3,9 +3,6 @@
 
 #include "state/State.hpp"
 #include "World.hpp"
-#include "entity/Overlay.hpp"
-#include "gui/Button.hpp"
-#include "gui/Menu.hpp"
 #include "entity/LevelCover.hpp"
 
 class Level : public State
@@ -13,29 +10,21 @@ class Level : public State
     public:
         Level(Stack& stack, const Assets& assets, const World& world);
 
-        void initialize();
-
         virtual void mouseReleased(const sf::Vector2f& position);
-        virtual void restart();
+        virtual void keyPressed(sf::Keyboard::Key key);
+        virtual void lostFocus();
 
         virtual void update(sf::Time deltaTime);
 
         virtual void draw(sf::RenderTarget& target) const;
 
-        virtual void mouseMoved(const sf::Vector2f& position);
-
         bool checkLost();
         void collisions();
-        void nextLevel();
-        void menu();
 
     private:
         bool m_launched;
-        const World& m_initialWorld;
         World m_world;
-        Overlay m_overlay;
         LevelCover m_levelCover;
-        std::vector<Menu> m_menus;
 };
 
 #endif
--- src/state/Level.cpp
+++ src/state/Level.cpp
@@ -3,103 +3,12 @@
 #include "state/LevelChoice.hpp"
 
 Level::Level(Stack& stack, const Assets& assets, const World& world)
- : State(stack, assets), m_launched(false),
-   m_initialWorld(world),
-   m_world(world),
-   m_menus({
-    Menu(sf::Vector2f(340.f,500.f),
-    L"Pause",
-    assets.font,
-    {
-    Button(300.f,L"Restart", assets.font),
-    Button(300.f,L"Resume", assets.font),
-    Button(300.f,L"Level Menu", assets.font)
-   }),
-   Menu(sf::Vector2f(1024.f,400.f),
-    L"Loose",
-    assets.font,
-    {
-    Button(300.f,L"Retry", assets.font),
-    Button(300.f,L"Level Menu", assets.font)
-   }),
-   Menu(sf::Vector2f(340.f,500.f),
-    L"Win !",
-    assets.font,
-    {
-    Button(300.f,L"Next Level", assets.font),
-    Button(300.f,L"Retry", assets.font),
-    Button(300.f,L"Level Menu", assets.font)
-   })
-})
-
-{
-    m_menus[0].setPosition(sf::Vector2f(342.f,43.f));
-    m_menus[2].setPosition(sf::Vector2f(342.f,43.f));
-}
-
-void Level::initialize()
-{
-    m_launched = false;
-    m_world = m_initialWorld;
-    m_overlay = Overlay();
-    m_levelCover = LevelCover();
-}
+ : State(stack, assets), m_launched(false), m_world(world)
+{}
 
 void Level::mouseReleased(const sf::Vector2f& position)
 {
-    int idButtonClicked;
-    if(isPaused())
-    {
-        if(m_menus[0].numButtonClicked(position, idButtonClicked))
-        {
-            switch(idButtonClicked)
-            {
-                case 0:
-                    restart();
-                    break;
-                case 1:
-                    resume();
-                    break;
-                case 2:
-                    menu();
-                    break;
-            }
-        }
-    }
-    else if(isLost())
-    {
-        if(m_menus[1].numButtonClicked(position, idButtonClicked))
-        {
-            switch(idButtonClicked)
-            {
-                case 0:
-                    restart();
-                    break;
-                case 1:
-                    menu();
-                    break;
-            }
-        }
-    }
-    else if(isWon())
-    {
-        if(m_menus[2].numButtonClicked(position, idButtonClicked))
-        {
-            switch(idButtonClicked)
-            {
-                case 0:
-                    nextLevel();
-                    break;
-                case 1:
-                    restart();
-                    break;
-                case 2:
-                    menu();
-                    break;
-            }
-        }
-    }
-    else if(!m_launched)
+    if(!m_launched)
     {
         for(Ball& b : m_world.balls)
             b.launch(position);
@@ -107,50 +16,44 @@ void Level::mouseReleased(const sf::Vector2f& position)
     }
 }
 
-void Level::mouseMoved(const sf::Vector2f& position)
+void Level::keyPressed(sf::Keyboard::Key key)
 {
-    for(Menu& m : m_menus)
-    {
-        m.changeColor(position);
-    }
+    if(key == sf::Keyboard::Space)
+        pop(); // restart
+    else if(key == sf::Keyboard::Escape)
+        ; // TODO : push(Pause)
 }
 
-void Level::restart()
+void Level::lostFocus()
 {
-    initialize();
-    resume();
+    // TODO : push(Pause);
 }
 
 void Level::update(sf::Time deltaTime)
 {
     m_levelCover.update(deltaTime);
-    if(isNotRunning()){
-        m_overlay.update(deltaTime);
+
+    for(Ball& b : m_world.balls)
+        b.update(deltaTime);
+
+    for(Magnet& m : m_world.magnets)
+    {
+        m.update(deltaTime);
+        for(Ball& b : m_world.balls)
+            m.interact(b, deltaTime);
     }
+
+    collisions();
+
+    if(checkLost())
+        ; // TODO: push(End)
     else
     {
+        bool contains = true;
         for(Ball& b : m_world.balls)
-            b.update(deltaTime);
-
-        for(Magnet& m : m_world.magnets)
-        {
-            m.update(deltaTime);
-            for(Ball& b : m_world.balls)
-                m.interact(b, deltaTime);
-        }
-
-        collisions();
-
-        if(checkLost())
-            lose();
-        else
-        {
-            bool contains = true;
-            for(Ball& b : m_world.balls)
-                contains = contains && m_world.hole.contains(b);
-            if(contains)
-                win();
-        }
+            contains = contains && m_world.hole.contains(b);
+        if(contains)
+            ; // TODO: push(End)
     }
 }
 
@@ -164,23 +67,6 @@ void Level::draw(sf::RenderTarget& target) const
     //target.draw(m_hole);
     for(const Ball& b : m_world.balls)
         target.draw(b);
-
-    if(isNotRunning())
-    {
-        target.draw(m_overlay);
-        if(isPaused())
-        {
-            target.draw(m_menus[0]);
-        }
-        if(isLost())
-        {
-            target.draw(m_menus[1]);
-        }
-        if(isWon())
-        {
-            target.draw(m_menus[2]);
-        }
-    }
 }
 
 bool Level::checkLost()
@@ -201,15 +87,4 @@ void Level::collisions()
         for(Ball& b : m_world.balls)
             if(b.intersects(w, hitPoint, normal))
                 b.bounce(hitPoint, normal);
-}
-
-void Level::nextLevel()
-{
-    pop();
-    //getStack().top() -> nextLevel();
-}
-
-void Level::menu()
-{
-    pop();
 }
