21f8ae71941620092af344e57c8bf806cc79f00d
kimci86 <kimci86@hotmail.fr>
2017-10-29 14:41:22 +0100

    Factor out level menus in a base state

--- include/state/Pause.hpp
+++ include/state/Pause.hpp
@@ -1,26 +1,14 @@
 #ifndef PAUSE_HPP
 #define PAUSE_HPP
 
-#include "state/State.hpp"
-#include "entity/Overlay.hpp"
-#include "gui/Menu.hpp"
+#include "state/LevelMenu.hpp"
 
-class Pause : public State
+class Pause : public LevelMenu
 {
     public:
         Pause(Stack& stack, const Assets& assets, const State& pausedState);
 
-        virtual void mouseMoved(const sf::Vector2f& position);
-        virtual void mouseReleased(const sf::Vector2f& position);
-
-        virtual void update(sf::Time deltaTime);
-
-        virtual void draw(sf::RenderTarget& target) const;
-
-    private:
-        const State& m_pausedState;
-        Overlay m_overlay;
-        Menu m_menu;
+        virtual void buttonClicked(int button);
 };
 
 #endif
--- src/state/Pause.cpp
+++ src/state/Pause.cpp
@@ -1,44 +1,27 @@
 #include "state/Pause.hpp"
 
 Pause::Pause(Stack& stack, const Assets& assets, const State& pausedState)
- : State(stack, assets), m_pausedState(pausedState),
-   m_menu({340.f,500.f}, L"Pause", assets.font,
+ : LevelMenu(stack, assets, pausedState,
+   {{340.f,500.f}, L"Pause", assets.font,
     {
         Button(300.f,L"Restart", assets.font),
         Button(300.f,L"Resume", assets.font),
         Button(300.f,L"Map", assets.font)
-    })
+    }})
 {}
 
-void Pause::mouseMoved(const sf::Vector2f& position)
+void Pause::buttonClicked(int button)
 {
-    m_menu.changeColor(position);
-}
-
-void Pause::mouseReleased(const sf::Vector2f& position)
-{
-    int idButton;
-    if(m_menu.numButtonClicked(position, idButton))
-        switch(idButton)
-        {
-            case 0: // restart
-                pop(2);
-                break;
-            case 1: // resume
-                pop();
-                break;
-            case 2: // map
-                pop(4);
-                break;
-        }
-}
-
-void Pause::update(sf::Time deltaTime)
-{}
-
-void Pause::draw(sf::RenderTarget& target) const
-{
-    m_pausedState.draw(target);
-    target.draw(m_overlay);
-    target.draw(m_menu);
+    switch(button)
+    {
+        case 0: // restart
+            pop(2);
+            break;
+        case 1: // resume
+            pop();
+            break;
+        case 2: // map
+            pop(4);
+            break;
+    }
 }
