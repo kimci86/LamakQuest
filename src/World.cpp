2650b15352869a4faabb64fadf91d27d0f379059
kimci86 <kimci86@hotmail.fr>
2017-10-29 15:21:01 +0100

    Only one ball in the world

--- src/World.cpp
+++ src/World.cpp
@@ -16,35 +16,32 @@ World::World(const std::string& filename)
         magnets.push_back(Magnet(100.f, 4, 2, sf::Color::Blue));
         magnets.back().setPosition(p1);
 
+        ifs >> p1.x >> p1.y;
+        ball.setPosition(p1);
+
         while(ifs)
         {
-            ifs >> c;
-            switch(c)
-            {
-                case 'B':
-                    ifs >> p1.x >> p1.y;
-                    balls.push_back(Ball());
-                    balls.back().setPosition(p1);
-                    break;
+            if(ifs >> c)
+                switch(c)
+                {
+                    case 'W':
+                        ifs >> p1.x >> p1.y >> p2.x >> p2.y;
+                        walls.push_back(Wall(p1, p2));
+                        break;
 
-                case 'W':
-                    ifs >> p1.x >> p1.y >> p2.x >> p2.y;
-                    walls.push_back(Wall(p1, p2));
-                    break;
+                    case 'M':
+                        {
+                            float intensity;
+                            sf::Uint32 color;
+                            ifs >> p1.x >> p1.y >> intensity >> std::hex >> color >> std::dec;
+                            magnets.push_back(Magnet(100.f, 4, intensity, sf::Color((color << 8) + 0xff)));
+                            magnets.back().setPosition(p1);
+                        }
+                        break;
 
-                case 'M':
-                    {
-                        float intensity;
-                        sf::Uint32 color;
-                        ifs >> p1.x >> p1.y >> intensity >> std::hex >> color >> std::dec;
-                        magnets.push_back(Magnet(100.f, 4, intensity, sf::Color((color << 8) + 0xff)));
-                        magnets.back().setPosition(p1);
-                    }
-                    break;
-
-                default:
-                    throw Error(std::string("Unknow entity type ")+c);
-            }
+                    default:
+                        throw Error(std::string("Unknow entity type \"")+c+"\"");
+                }
         }
     }
     else
